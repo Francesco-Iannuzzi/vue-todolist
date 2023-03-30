1c8c0d41b09f42eb94fc516854846fc048208354
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-30 15:18:28 +0200

    add keyup.enter on input

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. ✓
 
 Bonus:
-- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista ✓
 - cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
 */
